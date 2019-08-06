59a2322498c944bf158c4e65e8feb36c4cc92c85
Saket Satta <saket.satta@autodesk.com>
2019-08-05 17:58:36 -0700

    Add the basic demo functionality

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ABOUT</h2>
+        <p>CAS rules!
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default About;
---
+++ src/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Demo from "./Demo";
+import About from "./About";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>Intensity Plots</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/demo">Demo</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/demo" component={Demo}/>
+            <Route path="/about" component={About}/> 
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+ 
+export default Main;
